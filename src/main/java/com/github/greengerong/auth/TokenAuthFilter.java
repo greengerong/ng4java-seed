a5ffd0ff73cd8cda77ac9ac34604ccfc794c71cd
greengerong <grzx2210@163.com>
2014-07-15 13:36:57 +0800

    [green] create basic project struct

---
+++ src/main/java/com/github/greengerong/auth/TokenAuthFilter.java
@@ -0,0 +1,15 @@
+package com.github.greengerong.auth;
+
+/**
+ * ***************************************
+ * *
+ * Auth: green gerong                     *
+ * Date: 2014                             *
+ * blog: http://greengerong.github.io/    *
+ * github: https://github.com/greengerong *
+ * *
+ * ****************************************
+ */
+public class TokenAuthFilter {
+
+}
---
+++ src/main/webapp/scripts/feature/order/controller/OrderCtrl.js
@@ -0,0 +1,8 @@
+/******************************************
+ *                                        *
+ * Auth: green gerong                     *
+ * Date: 2014                             *
+ * blog: http://greengerong.github.io/    *
+ * github: https://github.com/greengerong *
+ *                                        *
+ ******************************************/
